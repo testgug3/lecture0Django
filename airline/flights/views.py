1317b87d121c463d5972d335b4d193665974ecd6
Rahul <testgug3@gmail.com>
2018-12-21 21:20:28 +0545

    airline many to may relationship django orm

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,31 @@ def index(request):
         "flights": Flight.objects.all()
     }
     return render(request, "flights/index.html", context)
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight does not exist")
+    context = {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    }
+    return render(request, "flights/flights.html", context)
+
+
+def book(request, flight_id):
+    try:
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        flight = Flight.objects.get(pk=flight_id)
+    except KeyError:
+        return render(request, "flights/error.html", {"message:" "No selection."})
+    except Flight.DoesNotExist:
+        return render(request, "flights/error.html", {"message:" "No flight."})
+    except Passenger.DoesNotExist:
+        return render(request, "flights/error.html", {"message:" "No passenger."})
+    
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
